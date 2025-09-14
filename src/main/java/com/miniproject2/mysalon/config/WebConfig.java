ba9db4c88a98f5627173eccdcb001349506c9c31
Sihoon Park <129057192+cooperear@users.noreply.github.com>
2025-09-15 08:30:08 +0900

    product load

--- src/main/java/com/miniproject2/mysalon/config/WebConfig.java
+++ src/main/java/com/miniproject2/mysalon/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.miniproject2.mysalon.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -13,5 +14,12 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PATCH", "PUT","DELETE", "OPTIONS")
                 .allowCredentials(true);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // 업로드된 파일들을 웹에서 접근 가능하도록 설정
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:C:/MySalon/uploads/");
     }
 }
